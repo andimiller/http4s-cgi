08a40e973cd350f24ea68bbf4763e21402748f5c
Andi Miller <andi@andimiller.net>
2022-12-31 02:38:55 +0000

    comment the chat example to show how it works

--- examples/websocket-chat/src/main/scala/WebsocketChat.scala
+++ examples/websocket-chat/src/main/scala/WebsocketChat.scala
@@ -30,38 +30,49 @@ object ChatLog {
 object WebsocketChat extends WebsocketdApp {
   override def create: WebSocketBuilder[IO] => HttpApp[IO] = ws => {
     HttpRoutes
-      .of[IO] { case Name(name) =>
+      .of[IO] { case Name(name) => // each user should provide a name as a query parameter
         ws.build { input =>
-          val in  = input
+          val in = input
             .collect { case WebSocketFrame.Text(text, _) => text }
-            .evalMap { s => IO { ChatLog(Instant.now(), name, s).asJson.noSpaces } }
+            .evalMap { s =>
+              IO { ChatLog(Instant.now(), name, s).asJson.noSpaces }
+            } // inputs are put into a ChatLog object with a timestamp and the user who said it
             .evalMap { msg =>
+              // Loop over all the sockets and message the ones we find
               Files[IO]
                 .list(FPath("./chatsockets/"))
                 .map(_.toString)
                 .filter(_.endsWith("chat.sock"))
                 .evalTap { handle =>
-                  UnixSockets[IO].client(UnixSocketAddress(handle)).use { sock =>
-                    fs2.Stream.emit(msg).through(fs2.text.utf8.encode[IO]).through(sock.writes).compile.drain
-                  }
+                  UnixSockets[IO]
+                    .client(UnixSocketAddress(handle))
+                    .use { sock =>
+                      fs2.Stream.emit(msg).through(fs2.text.utf8.encode[IO]).through(sock.writes).compile.drain
+                    }
+                    .attempt // sometimes there's a stale socket and we'd like to just pretend everything's okay in this demo
+                    .void
                 }
                 .compile
                 .drain
             }
+
+          // our outputs are purely what's sent to our socket
           val out = UnixSockets[IO]
             .server(UnixSocketAddress(s"./chatsockets/$name.chat.sock"), deleteIfExists = true, deleteOnClose = true)
-            .flatMap { sock =>
+            .flatMap { sock =>               // each new connection is another process sending us a message
               sock.reads
                 .through(fs2.text.utf8.decode[IO])
-                .through(fs2.text.lines[IO])
-                .evalMap { s => IO.fromEither(io.circe.parser.parse(s).flatMap(_.as[ChatLog])) }
+                .through(fs2.text.lines[IO]) // they should be line delimited
+                .evalMap { s =>
+                  IO.fromEither(io.circe.parser.parse(s).flatMap(_.as[ChatLog]))
+                }                            // they should be ChatLogs serialized as JSON
                 .map { chatLog =>
                   WebSocketFrame.Text(
-                    s"[${chatLog.time}] <${chatLog.name}> ${chatLog.message}"
+                    s"[${chatLog.time}] <${chatLog.name}> ${chatLog.message}" // present it nicely for the user
                   )
                 }
             }
-            .onFinalize(Files[IO].delete(FPath(s"./chatsockets/$name.chat.sock")))
+            .onFinalize(Files[IO].delete(FPath(s"./chatsockets/$name.chat.sock"))) // delete our socket if we left it by accident
           out.concurrently(in)
         }
       }
