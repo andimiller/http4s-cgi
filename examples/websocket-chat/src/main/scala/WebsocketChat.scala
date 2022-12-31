a202db845a53c81fbd67d24c7674af6b83ef9309
Andi Miller <andi@andimiller.net>
2022-12-31 01:53:53 +0000

    swap to bare minimum socket read

--- examples/websocket-chat/src/main/scala/WebsocketChat.scala
+++ examples/websocket-chat/src/main/scala/WebsocketChat.scala
@@ -30,14 +30,22 @@ object ChatLog {
   implicit val codec: Codec[ChatLog] = deriveCodec
 }
 
+object ReadSocket extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] =
+    UnixSockets[IO].server(UnixSocketAddress("/tmp/test.sock")).compile.resource.lastOrError.use { sock =>
+      sock.reads.through(fs2.text.utf8.decode[IO]).through(fs2.text.lines[IO]).evalMap(IO.println(_)).compile.drain.as(ExitCode.Success)
+    }
+}
+
 /** This is an example websocket server which lets people join and chat to each other via redis
   */
+/*
 object WebsocketChat extends WebsocketdApp {
   override def create: WebSocketBuilder[IO] => HttpApp[IO] = ws => {
     HttpRoutes
       .of[IO] { case Name(name) =>
         UnixSockets[IO]
-          .server(UnixSocketAddress(s"./$name.sock"), deleteIfExists = true, deleteOnClose = true)
+          .server(UnixSocketAddress(s"/tmp/chat.$name.sock"), deleteIfExists = true, deleteOnClose = true)
           .compile
           .resource
           .lastOrError
@@ -51,3 +59,6 @@ object WebsocketChat extends WebsocketdApp {
       .orNotFound
   }
 }
+
+
+ */
