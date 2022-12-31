127cbc1353c162342e14cdac52e4e0f410aa47e4
Andi Miller <andi@andimiller.net>
2022-12-31 02:26:56 +0000

    full implementation with unix sockets

--- examples/websocket-chat/src/main/scala/WebsocketChat.scala
+++ examples/websocket-chat/src/main/scala/WebsocketChat.scala
@@ -1,9 +1,5 @@
-import cats.effect.kernel.Resource
 import cats.effect._
-import cats.effect.std.{Dispatcher, Queue}
-import cats.implicits._
-import fs2.concurrent.Topic
-import fs2.io.net.Socket
+import fs2.io.file.{Files, Path => FPath}
 import fs2.io.net.unixsocket.{UnixSocketAddress, UnixSockets}
 import io.circe.Codec
 import io.circe.generic.semiauto._
@@ -15,7 +11,6 @@ import org.http4s.server.websocket.WebSocketBuilder
 import org.http4s.websocket.WebSocketFrame
 
 import java.time.Instant
-import scala.concurrent.duration.{DurationDouble, DurationInt}
 
 object Name {
   def unapply[F[_]](req: Request[F]): Option[String] =
@@ -30,35 +25,46 @@ object ChatLog {
   implicit val codec: Codec[ChatLog] = deriveCodec
 }
 
-object ReadSocket extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] =
-    UnixSockets[IO].server(UnixSocketAddress("/tmp/test.sock")).compile.resource.lastOrError.use { sock =>
-      sock.reads.through(fs2.text.utf8.decode[IO]).through(fs2.text.lines[IO]).evalMap(IO.println(_)).compile.drain.as(ExitCode.Success)
-    }
-}
-
 /** This is an example websocket server which lets people join and chat to each other via redis
   */
-/*
 object WebsocketChat extends WebsocketdApp {
   override def create: WebSocketBuilder[IO] => HttpApp[IO] = ws => {
     HttpRoutes
       .of[IO] { case Name(name) =>
-        UnixSockets[IO]
-          .server(UnixSocketAddress(s"/tmp/chat.$name.sock"), deleteIfExists = true, deleteOnClose = true)
-          .compile
-          .resource
-          .lastOrError
-          .use { case inSock =>
-            ws.build { input =>
-              inSock.reads.through(fs2.text.utf8.decode[IO]).through(fs2.text.lines[IO]).map(WebSocketFrame.Text(_))
+        ws.build { input =>
+          val in  = input
+            .collect { case WebSocketFrame.Text(text, _) => text }
+            .evalMap { s => IO { ChatLog(Instant.now(), name, s).asJson.noSpaces } }
+            .evalMap { msg =>
+              Files[IO]
+                .list(FPath("/tmp"))
+                .map(_.toString)
+                .filter(_.endsWith("chat.sock"))
+                .evalTap { handle =>
+                  UnixSockets[IO].client(UnixSocketAddress(handle)).use { sock =>
+                    fs2.Stream.emit(msg).through(fs2.text.utf8.encode[IO]).through(sock.writes).compile.drain
+                  }
+                }
+                .compile
+                .drain
             }
-          }
-
+          val out = UnixSockets[IO]
+            .server(UnixSocketAddress(s"/tmp/$name.chat.sock"), deleteIfExists = true, deleteOnClose = true)
+            .flatMap { sock =>
+              sock.reads
+                .through(fs2.text.utf8.decode[IO])
+                .through(fs2.text.lines[IO])
+                .evalMap { s => IO.fromEither(io.circe.parser.parse(s).flatMap(_.as[ChatLog])) }
+                .map { chatLog =>
+                  WebSocketFrame.Text(
+                    s"[${chatLog.time}] <${chatLog.name}> ${chatLog.message}"
+                  )
+                }
+            }
+            .onFinalize(Files[IO].delete(FPath(s"/tmp/$name.chat.sock")))
+          out.concurrently(in)
+        }
       }
       .orNotFound
   }
 }
-
-
- */
